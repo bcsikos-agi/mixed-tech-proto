4bee696ea7e6d167fe653dbed786013b9092d544
Balint Csikos <bcsikos@arisglobal.com>
2023-06-09 18:36:43 +0200

    Mods for routing - left(ang)

--- left-sidebar/src/app/app.module.ts
+++ left-sidebar/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'first', component: FirstComponent},
+      {path: 'second', component: SecondComponent},
+    ]),
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
